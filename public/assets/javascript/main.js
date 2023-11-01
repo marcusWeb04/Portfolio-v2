08e1891491d25710d438827104b6681bd0af3880
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-11-01 16:58:21 +0100

    Add files via upload

---
+++ public/assets/javascript/main.js
@@ -0,0 +1,27 @@
+// apparition de la modal pour les parametres
+function appearsParameter(){
+    document.querySelector(".parameter").classList.toggle("appears");
+}
+
+// apparition de la modal pour les parametres
+function appearsProjectModal(){
+    document.querySelector(".modal-project").classList.add("appears");
+}
+function desappearProjectModal(){
+    document.querySelector(".modal-project").classList.remove("appears");
+}
+
+// apparition pour les projets
+function appears(nom) {
+    var docs = document.querySelectorAll(".part-project");
+    docs.forEach((doc) => doc.classList.remove("appears"));
+    if(nom == "personal"){
+        document.querySelector('#personal-project').classList.add("appears");
+    }
+    else if(nom == "profesional-project"){
+        document.querySelector('#profesional-project').classList.add("appears");
+    }
+    else if(nom = "supervise-project"){
+        document.querySelector('#supervise-project').classList.add("appears");   
+    }
+}
